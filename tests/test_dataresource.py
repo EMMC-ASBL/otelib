e026f346ec3d4b2f8d8fbb22cdd234e28fddba61
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-02-08 09:59:50 +0100

    Update test using `requests_mock`
    The point of mocking HTTP responses from an OTE server is to ensure the
    otelib is tested and not the OTE server.

--- tests/test_dataresource.py
+++ tests/test_dataresource.py
@@ -1,11 +1,16 @@
 """Test parse strategies."""
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from requests_mock import Mocker
 
 
-def test_dataresource():
+def test_dataresource(requests_mock: "Mocker") -> None:
     """Test dataresource parse strategy."""
     import json
 
-    from otelib import OntoTransServer
+    from otelib.ontotransserver import OntoTransServer
+    from otelib.settings import Settings
 
     data = {
         "firstName": "Joe",
@@ -15,7 +20,38 @@ def test_dataresource():
         "address": {"streetAddress": "101", "city": "San Diego", "state": "CA"},
         "phoneNumbers": [{"type": "home", "number": "7349282382"}],
     }
-    server = OntoTransServer("http://localhost:80")
+    resource_id = {"resource_id": "dataresource-test"}
+    session_id = {"session_id": "sessions-test"}
+    url_prefix = Settings().prefix
+
+    server = OntoTransServer("http://example.org")
+
+    # DataResource.create()
+    requests_mock.post(
+        f"{server.url}{url_prefix}/dataresource/",
+        text=json.dumps(resource_id),
+    )
+
+    # AbstractStrategy.get() (DataResource.get())
+    requests_mock.post(
+        f"{server.url}{url_prefix}/session/",
+        text=json.dumps(session_id),
+    )
+
+    # DataResource.initialize()
+    requests_mock.post(
+        f"{server.url}{url_prefix}/dataresource/{resource_id['resource_id']}/"
+        f"initialize?session_id={session_id['session_id']}",
+        text="{}",
+    )
+
+    # DataResource.fetch()
+    requests_mock.get(
+        f"{server.url}{url_prefix}/dataresource/{resource_id['resource_id']}"
+        f"?session_id={session_id['session_id']}",
+        text=json.dumps(data),
+    )
+
     dataresource = server.create_dataresource(
         downloadUrl="https://filesamples.com/samples/code/json/sample2.json",
         mediaType="text/json",
