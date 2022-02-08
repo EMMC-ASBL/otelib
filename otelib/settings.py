c43e3d3fa5c2936ba622dc693479541652a93f95
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-02-08 16:17:00 +0100

    Use local docker-compose file for CI test
    Avoid cloning in the complete oteapi-services repository and instead
    have a local docker-compose file to use for the CI test with a real
    backend.
    
    Set the same environment variable prefix for the otelib settings as is
    set for oteapi-services (`OTEAPI_`).

--- otelib/settings.py
+++ otelib/settings.py
@@ -7,3 +7,8 @@ class Settings(BaseModel):
     """Configuration settings for an OTE client."""
 
     prefix: str = Field("/api/v1", description="Application route prefix.")
+
+    class Config:
+        """Pydantic configuration class."""
+
+        env_prefix = "OTEAPI_"
