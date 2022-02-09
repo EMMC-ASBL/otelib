1828670bf48fed112ab45b4f5ad1ecf6a145d47b
Treesarj <66374813+Treesarj@users.noreply.github.com>
2022-02-09 10:01:20 +0100

    Rename OntoTranServer to OTEClient (#8)
    Update README.
    
    Remove compevo example - it should not be on GitHub.
    
    Co-authored-by: Casper Welzel Andersen <casper.w.andersen@sintef.no>

--- otelib/__init__.py
+++ otelib/__init__.py
@@ -1,9 +1,4 @@
-from .dataresource import DataResource
-from .filter import Filter
-from .transformation import Transformation
-from .pipe import Pipe
-from .mapping import Mapping
-from .ontotransserver import OntoTransServer
+from .client import OTEClient
 
 
-__all__ = ("DataResource", "Filter", "Transformation", "Pipe", "Mapping", "OntoTransServer")
+__all__ = ("OTEClient",)
--- otelib/client.py
+++ otelib/client.py
@@ -4,8 +4,8 @@ from otelib.filter import Filter
 from otelib.mapping import Mapping
 
 
-class OntoTransServer:
-    """The OntoTransServer object represent a remote OntoTrans REST API on the network."""
+class OTEClient:
+    """The OTEClient object represent a remote OntoTrans REST API on the network. """
 
     def __init__(self, url):
         self.url = url
