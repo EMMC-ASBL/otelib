cba51dd6e7e06a466e74fcbda4f2a6f5e96ec54f
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-03-14 15:43:55 +0100

    Update otelib API
    Move strategies into separate sub-module.
    Update typing information.
    Use `id` instead of `id_` for the strategy attribute.
    Update example notebook accordingly.
    Only make `OTEClient` importable straight from `otelib`.

--- otelib/__init__.py
+++ otelib/__init__.py
@@ -3,9 +3,8 @@
 A package to interact with the Open Translation Environment (OTE) API.
 """
 from .client import OTEClient
-from .pipe import Pipe
 
-__all__ = ("OTEClient", "Pipe")
+__all__ = ("OTEClient",)
 
 __version__ = "0.0.1"
 __author__ = "SINTEF"
--- otelib/pipe.py
+++ otelib/pipe.py
@@ -1,19 +1,16 @@
 """Pipe object for creating a pipeline."""
-# pylint: disable=too-few-public-methods
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Any, Dict
-
-    from oteapi.interfaces import IStrategy
+    from otelib.strategies.abc import AbstractStrategy
 
 
 class Pipe:
     """Pipe object in a pipe-and-filter pattern."""
 
-    def __init__(self, strategy: "IStrategy") -> None:
-        self.input = strategy
+    def __init__(self, strategy: "AbstractStrategy") -> None:
+        self.input: "AbstractStrategy" = strategy
 
-    def get(self, session: "Dict[str, Any]") -> "Dict[str, Any]":
+    def get(self, session: str) -> bytes:
         """Call the input strategy's `get()` method."""
         return self.input.get(session)
