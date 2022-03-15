c1f335368c0abdfdd5b1e099fd4c2e0231f73fc8
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-03-15 18:34:13 +0100

    Skip function tests where there is not func strat

--- tests/strategies/test_abc.py
+++ tests/strategies/test_abc.py
@@ -29,6 +29,9 @@ def test_get(
     create_kwargs: "Dict[str, Any]",
 ) -> None:
     """Test `AbstractStrategy.get()`."""
+    if strategy_name == "function" and "example" not in server_url:
+        pytest.skip("No function strategy exists in oteapi-core yet.")
+
     import json
 
     import requests
@@ -77,15 +80,10 @@ def test_get(
         return_json=testdata(strategy_name),
     )
 
-    strategy_name_map = {
-        "dataresource": "DataResource",
-        "filter": "Filter",
-        "mapping": "Mapping",
-        "transformation": "Transformation",
-    }
+    strategy_name_map = {"dataresource": "DataResource"}
 
     strategy: "AbstractStrategy" = getattr(
-        strategies, strategy_name_map[strategy_name]
+        strategies, strategy_name_map.get(strategy_name, strategy_name.capitalize())
     )(server_url)
 
     # We must create the strategy - getting a strategy ID
@@ -148,15 +146,10 @@ def test_get_fails(
         content=b"Internal Server Error",
     )
 
-    strategy_name_map = {
-        "dataresource": "DataResource",
-        "filter": "Filter",
-        "mapping": "Mapping",
-        "transformation": "Transformation",
-    }
+    strategy_name_map = {"dataresource": "DataResource"}
 
     strategy: "AbstractStrategy" = getattr(
-        strategies, strategy_name_map[strategy_name]
+        strategies, strategy_name_map.get(strategy_name, strategy_name.capitalize())
     )(server_url)
 
     # We must create the strategy - getting a strategy ID
