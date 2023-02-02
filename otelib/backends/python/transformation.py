f73c7277882353c67d334d180aee9c9b36bb7f73
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2023-02-02 13:42:07 +0100

    Handle testing mapping strategy specially
    This is due to the "triples" field being of Set type and also a set of
    tuples.
    The latter pertains to testing the Python backend and the former
    pertains to both backends, since a set is unordered, comparisons with
    test data is tricky.

--- otelib/backends/python/transformation.py
+++ otelib/backends/python/transformation.py
@@ -6,7 +6,7 @@ from oteapi.models import TransformationConfig
 from otelib.backends.python.base import BasePythonStrategy
 
 if TYPE_CHECKING:  # pragma: no cover
-    from typing import Optional
+    from typing import Type
 
 
 class Transformation(BasePythonStrategy):
@@ -14,4 +14,4 @@ class Transformation(BasePythonStrategy):
     operations."""
 
     strategy_name = "transformation"
-    strategy_config = TransformationConfig
+    strategy_config: "Type[TransformationConfig]" = TransformationConfig
