61f538a77291936fb8dc53c1fca23347691a4ac6
Matthias Büschelberger <matthias.bueschelberger@iwm.fraunhofer.de>
2023-03-21 12:01:31 +0100

    remove obsolete settings-attributes for default host (ote-services)

--- otelib/settings.py
+++ otelib/settings.py
@@ -1,8 +1,6 @@
 """Configuration settings for creating the OTE client."""
 from pydantic import BaseModel, Field
 
-DEFAULT_HOST = "http://localhost:8080"
-
 
 class Settings(BaseModel):
     """Configuration settings for an OTE client."""
@@ -10,14 +8,6 @@ class Settings(BaseModel):
     prefix: str = Field("/api/v1", description="Application route prefix.")
     timeout: tuple[float, float] = Field(
         (3.0, 27.0), description="Tuple for URL connect and read timeouts in seconds."
-    )
-
-    remote_backend: str = Field(
-        DEFAULT_HOST,
-        description=f"""In case the otelib is supposed to connect to ote-services
-        as backend instead of the oteapi-core in the local Python-environment, a host-url
-        (included ports) can be set here.
-        This might be e.g. {DEFAULT_HOST} (default) or e.g. https://ote.ontotrans.eu""",
     )
 
     class Config:
